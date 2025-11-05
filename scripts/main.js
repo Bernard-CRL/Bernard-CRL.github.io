9d8915879f9f0d35066f96f5961cb1fe0e262db6
Bernard-CRL <Andrew.Massola@outlook.com>
2025-11-05 18:04:57 -0500

    Added a new function for To-Do list

--- scripts/main.js
+++ scripts/main.js
@@ -35,3 +35,17 @@ if (!localStorage.getItem("name")){
 myButton.addEventListener("click",()=>{
     setUsername()
 })
+
+const listItems=document.querySelector("#todo li")
+function toggleDone(e ){
+    if (!e.target.className){
+        e.target.className ="done";
+
+    }else{
+        e.target.className ="";
+    }
+}
+
+listItems.forEach((item)=>{
+    item.addEventListener("click",toggleDone)
+})
